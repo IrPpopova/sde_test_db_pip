19b755d52852cb246cdd3bfac3388c852bbd4c86
IrPpopova <guinea-pigs@yandex.ru>
2022-03-13 23:19:05 +0300

    test

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+$ docker pull postgres
+$ docker run --name pip-postgres -v "$(pwd)"/../sql:/Ipopova_sql -e POSTGRES_PASSWORD=@sde_password012 -e POSTGRES_USER=test_sde -e POSTGRES_DB=demo -d -p 5432:5432 postgres
+$ docker exec -it pip-postgres bash
+cd ../'\Program Files\Git\Ipopova_sql'
+# psql -U test_sde -d demo -f demo.sql
+
+
